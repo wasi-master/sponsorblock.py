58d3483f1bf925245606cb69eddc2641a061248a
Wasi Master <63045920+wasi-master@users.noreply.github.com>
2021-08-26 12:58:39 +0600

    Add more errrors

--- sponsorblock/errors.py
+++ sponsorblock/errors.py
@@ -1,11 +1,42 @@
-class InvalidJSONError(Exception):
+class InvalidJSONException(Exception):
     """Raised when the JSON gotten from the server is invalid"""
+
     def __init__(self, message, response):
         self.response = response
-        super().__init__(message + ': ' + response.text)
+        super().__init__(f"{message}: {response.text}")
+
 
 class HTTPException(Exception):
     """Raised when the server returns an error code"""
+
     def __init__(self, message, response):
         self.response = response
-        super().__init__(message + ': ' + response.status_code + response.reason)
+        super().__init__(f"{message}: {response.status_code} {response.reason}")
+
+
+class Forbidden(HTTPException):
+    """Raised if the status code is 403"""
+
+
+class BadRequest(HTTPException):
+    """Raised if the status code is 400"""
+
+
+class NotFoundException(HTTPException):
+    """Raised when the status code is 404"""
+
+
+class RateLimitException(HTTPException):
+    """Raised when the status code is 429"""
+
+
+class UnexpectedException(HTTPException):
+    """Raised if an unknown error has occurred."""
+
+
+class ServerException(HTTPException):
+    """Raised if the status code is bigger than 500"""
+
+
+class DuplicateException(HTTPException):
+    """Raised if the status code is 409"""
