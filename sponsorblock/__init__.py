b67c1b3858feb36d00cb7d99e69fdbc28c0b4706
Wasi Master <63045920+wasi-master@users.noreply.github.com>
2021-08-26 13:00:34 +0600

    Add more info in init.py

--- sponsorblock/__init__.py
+++ sponsorblock/__init__.py
@@ -1,3 +1,18 @@
-from .client import Client, AsyncClient
-from .classes import Segment
-from .errors import InvalidJSONError
+"""A wrapper for the sponsorblock API."""
+from .client import Client
+from .errors import (
+    BadRequest,
+    DuplicateException,
+    Forbidden,
+    HTTPException,
+    InvalidJSONException,
+    NotFoundException,
+    RateLimitException,
+    ServerException,
+    UnexpectedException,
+)
+from .models import Segment, User
+from .utils import SortType
+
+__name__ = "sponsorblock.py"  # noqa
+__version__ = "0.1.0"
